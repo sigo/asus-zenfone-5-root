cbdb5f4a9cc9b2f2a235afe497f050342f8b0ccc
Mateusz Jagiełło <spam@sigo.pl>
2015-06-05 22:12:20 +0200

    Root.sh development

--- root.sh
+++ root.sh
@@ -1,3 +1,42 @@
 #!/bin/bash
 
 cd $(dirname "${0}")
+
+adb wait-for-devices
+adb reboot bootloader
+sleep 20
+
+echo "Unlocking fastboot"
+echo "--- log ---"
+fastboot flash fastboot files/root/fastboot.img
+fastboot reboot-bootloader
+echo "--- end log ---"
+sleep 20
+
+echo "Unlocking bootloader"
+echo "--- log ---"
+fastboot flash dnx files/vanilla/dnx_fwr_ctp_a500cg.bin
+fastboot flash ifwi files/root/ifwi_ctp_a500cg.bin
+fastboot reboot-bootloader
+echo "--- end log ---"
+sleep 180
+
+echo "Rooting phone"
+echo "--- log ---"
+fastboot flash recovery files/root/recovery.img
+sleep 5
+fastboot flash update files/root/dummy.zip
+adb wait-for-devices
+adb reboot bootloader
+echo "--- end log ---"
+sleep 20
+
+echo "Restoring vanilla files"
+echo "--- log ---"
+fastboot flash fastboot files/vanilla/fastboot.img
+fastboot flash recovery files/vanilla/recovery.img
+sleep 5
+fastboot flash dnx files/vanilla/dnx_fwr_ctp_a500cg.bin
+fastboot flash ifwi files/vanilla/ifwi_ctp_a500cg.bin
+fastboot reboot
+echo "--- end log ---"
